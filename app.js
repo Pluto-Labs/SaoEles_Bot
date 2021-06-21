8b3d04b94b50dd52d52a65b386153cf1bfa730d5
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2021-06-21 11:51:06 -0300

    função para salvar foto

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-require('dotenv').config()
-const { Client, MessageAttachment  } = require('discord.js')
+var { v4: uuidv4 } = require('uuid');
+var dotenv = require('dotenv')
+var { Client, MessageAttachment, Message  } =  require('discord.js')
+var randomFile = require('select-random-file')
+var https = require('https')
+var fs = require('fs')
+var path = require('path')
+
+dotenv.config()
+
 const client = new Client()
-const randomFile = require('select-random-file')
+
+const download = function(url, dest, msg) {
+  var file = fs.createWriteStream(dest)
+  var request = https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close()
+      msg.channel.send("Foto adicionada :white_check_mark:")
+    })
+  }).on('error', function(err) {
+    msg.channel.send("Não foi possivel adicionar essa foto :x:")
+    fs.unlink(dest)
+  })
+}
 
 const dir = './src/images'
 
@@ -12,9 +33,14 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content.toUpperCase() === 'SÃO ELES') {
     randomFile(dir, (err, file) => {
-      console.log("FILE ===== ", file)
       const attachment = new MessageAttachment(`./src/images/${file}`)
       msg.channel.send(attachment)
+    })
+  } else if(msg.content === "!manitos") {
+    msg.attachments.forEach(a => {
+      const ext = path.extname(a.name)
+      const name = uuidv4()
+      download(a.url, `${dir}/${name}${ext}`, msg)
     })
   }
 })
